6d912c4c9d42a4555418a64a1af0a10acfbc693e
asish029 <53825404+asish029@users.noreply.github.com>
2020-03-08 18:59:11 +0530

    modified

--- dsljob.groovy
+++ dsljob.groovy
@@ -2,9 +2,7 @@ job('amazon-compile1') {
     scm {
         git('https://github.com/asish029/game-of-life.git')
     }
-    triggers {
-        scm('0 * * * *')
-    }
+    
     steps {
         maven('-e clean test')
     }
