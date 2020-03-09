a989e52a03807e9d5bf57f2ff8282c66cb20c251
asish029 <53825404+asish029@users.noreply.github.com>
2020-03-09 17:58:23 +0530

    modified

--- dsltest.groovy
+++ dsltest.groovy
@@ -7,5 +7,6 @@ job('amazon-test') {
         maven('-e clean package')
     }
     postBuildSteps('SUCCESS') {
-    }    shell("url -v -u admin:admin -T **/*.war 'http://127.0.1.1:8080/manager/text/deploy?path=/my_app&update=true'")
+       shell("url -v -u admin:admin -T **/*.war 'http://127.0.1.1:8080/manager/text/deploy?path=/my_app&update=true'")
+    }
 }
